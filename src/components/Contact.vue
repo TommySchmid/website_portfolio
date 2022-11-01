2bfe9f397979c7b9430082ba90cb7a0cf3020f04
Tommy <taschmid20@gmail.com>
2022-11-01 19:01:13 -0400

    Contact component styling

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -8,8 +8,9 @@
           A highly motivated and task-oriented self-starter seeking employment as a software engineer in order to build on my existing skills and grow into a well-rounded frontend developer.
         </div>
       </div>
-      <div> 
-        Contact Info
+      <div className="contactInfo">
+        <div><a href="https://github.com/TommySchmid">Github</a></div>
+        <div><a href="https://www.linkedin.com/in/thomas-schmid-a89729b2/">LinkedIn</a></div>
       </div>
     </div>
   </template>
@@ -26,11 +27,23 @@
   display: flex;
   flex-direction: row;
   width: 100%;
+  color: white;
+  background-color: rgb(17, 17, 17);
 }
 
 .contactContainer div {
-  margin: auto;
-  width: 75%;
+  margin: 15px;
+  width: 85%;
+}
+
+.contactInfo {
+  display: flex;
+  flex-direction: column;
+  text-align: right;
+}
+
+a {
+  color: #2ea44f;
 }
 
 </style>
